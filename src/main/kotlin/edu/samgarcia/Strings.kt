80910de1e0308c09ba7f2b6a4547eca9089f7e86
Sam Garcia <samgcbusiness@gmail.com>
2022-01-01 18:49:47 +0100

    🙈 Update .gitignore

---
+++ src/main/kotlin/edu/samgarcia/Strings.kt
@@ -0,0 +1,9 @@
+package edu.samgarcia
+
+object Constants {
+    const val ROOT_ENDPOINT_MSG = "Welcome to the One Piece API \uD83D\uDC52"
+    const val ALL_CHARACTERS_INVALID_PAGE_QUERY_MSG = "Only numbers allowed for page query. \uD83D\uDCA5"
+    const val ALL_CHARACTERS_EMPTY_PAGE_MSG = "No characters found."
+    const val OK = "OK"
+    const val PAGE_NOT_FOUND = "Page not found."
+}
